e62c8ab723b561a36c2eeb98cd96d37955a04406
TJ <tjchase34@gmail.com>
2019-03-26 19:47:16 -0400

    Twitter api setup and gitignore

---
+++ src/tw.py
@@ -0,0 +1,13 @@
+import twitter
+
+api = twitter.Api(consumer_key= 'oWiSM7BuSqq9Je0rddAWAyRDI',
+                consumer_secret= 'FY7I9s4xRkdbIIvSSXivZ948HJn3zd6m2C70rwFEenzEOI7kvK',
+                access_token_key= '1096150810478735360-yP5Z5pScHIGoGB0x4gPPaz06Yiqk3F',
+                access_token_secret= 'dpSSg6z3Kh5Xo3Abdg3CBtwefBI2AOEAszXSzCTMPjnua')
+
+# For example, if you want to search for only tweets containing the word “twitter”, then you could do the following:
+
+# results = api.GetSearch(
+#     raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
+
+                  

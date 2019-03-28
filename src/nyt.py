7f37cf94693a520688efc4c0871280c5f418a4dd
manmeet18singh <manmeets@buffalo.edu>
2019-03-28 17:44:19 -0400

    collected 700 articles with keyword March Madness

--- src/nyt.py
+++ src/nyt.py
@@ -1,18 +1,39 @@
 from nytimesarticle import articleAPI
-import jsonpickle
+import csv
+import os.path
+import time
 
 api = articleAPI('aruVhl4pt0AUEOnD9LR7V1s4p7wavw9H')
 
 # collect articles with March Madness Keyword
-mmArt = api.search(q = 'March Madness',
-                    fq = 'The New York Times')
+mmArt = None
+mmUrlCsv = ['url']
+
+# mmArt = api.search(q = 'March Madness',
+                    # fq = 'The New York Times', page = 70)
+
+# print(mmArt)
+
+# for article in mmArt['response']['docs']:
+    # print(article['web_url'])
+
+for pageNum in range(0, 70):
+    mmArt = api.search(q = 'March Madness',
+                    fq = 'The New York Times', page = pageNum)
+    # save mmArticle to csv format
+    for article in mmArt['response']['docs']:
+        mmUrlCsv.append(article['web_url'])
+    time.sleep(6)
 
 # collect articles with keywords Sport or Sports
 # sportArt = api.search(q = ['Sport', 'Sports'], 
                         # fq = {'headline' : ['Sport', 'Sports'], 'body':['Sport', 'Sports']}, 
                         # begin_date = 20190101)
 
-# Save collections as JSON file
-
-
-print(mmArt['response']['docs'])
+# save data to csv and write to csv in Data/nyt/
+fileName = os.path.join('../Data/nyt/mmUrl.csv')
+with open(fileName, 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    for url in mmUrlCsv: 
+        writer.writerow([url])
+csvFile.close()

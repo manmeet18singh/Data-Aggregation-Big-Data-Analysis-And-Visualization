ba9dee160656bd7d0c430eefe24e8d2fda333474
manmeet18singh <manmeets@buffalo.edu>
2019-03-29 00:42:42 -0400

    streamlined and debugged twitter data gathering... Im pretty sure this one works its working atm... yikes

--- src/tw.py
+++ src/tw.py
@@ -5,21 +5,38 @@ import time
 
 # set up Twitter api key info
 auth = tweepy.AppAuthHandler('oWiSM7BuSqq9Je0rddAWAyRDI','FY7I9s4xRkdbIIvSSXivZ948HJn3zd6m2C70rwFEenzEOI7kvK')
-# auth.set_access_token('1096150810478735360-yP5Z5pScHIGoGB0x4gPPaz06Yiqk3F','dpSSg6z3Kh5Xo3Abdg3CBtwefBI2AOEAszXSzCTMPjnua')
 
-api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+api = tweepy.API(auth, wait_on_rate_limit_notify = True)
 
 # Rate Limit Handler, restart every 15 mins after rate limit is exceeded
 def limitHandler(cursor):
+    firstWrite = False
+
     while True:
         try:
             yield cursor.next()
-        except tweepy.TweepError:
+        except (tweepy.TweepError, StopIteration):
+            print('hit rate limit ... saved this batch to csv ... sleeping for 15 mins')
+            
+            # save data to csv and write to csv in Data/tw/
+            if (not firstWrite):
+                fileName = os.path.join('../Data/tw/tweets.csv')
+                with open(fileName, 'w') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerows(csvData)
+                    firstWrite = True
+                csvFile.close()
+            else:
+                fileName = os.path.join('../Data/tw/tweets.csv')
+                with open(fileName, 'a') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerows(csvData)
+                csvFile.close()
             time.sleep(15*60)
-
+ 
 
 searchQuery = 'March Madness OR #marchmadness'
-maxTweets = 80000
+maxTweets = 60000
 
 tweetId = None
 numTweets = 0
@@ -27,13 +44,8 @@ csvData = [['Created At', 'Text', 'Name']]
 
 # search on Twitter with Keyword: March Madness, retrieve tweets
 for tweet in limitHandler(tweepy.Cursor(api.search, q = searchQuery).items(maxTweets)):
-    csvData.append([tweet.created_at.strftime("%m/%d/%Y"),tweet.text,tweet.user.name])
-    numTweets += 1
-    print("Downloaded {0} tweets".format(numTweets))
-
-# save data to csv and write to csv in Data/tw/
-fileName = os.path.join('../Data/tw/tweets.csv')
-with open(fileName, 'w') as csvFile:
-    writer = csv.writer(csvFile)
-    writer.writerows(csvData)
-csvFile.close()
+    if ('RT @' not in tweet.text):
+        csvData.append([tweet.created_at.strftime("%m/%d/%Y"),tweet.text,tweet.user.name])
+        numTweets += 1
+        if (numTweets%1000 == 0):
+            print("Downloaded {0} tweets".format(numTweets))

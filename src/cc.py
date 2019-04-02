0a14b15718b3afe77cd4157903f74048ed5bc116
TJ <tjchase34@gmail.com>
2019-04-02 13:41:35 -0400

    Added extracted warc strings

--- src/cc.py
+++ src/cc.py
@@ -1,20 +1,22 @@
 import requests
 import json
-from io import StringIO
+import io
 import gzip
 import s3fs
 from bs4 import BeautifulSoup
 
 # Domains to search through
-domains = ['espn.com', 'cnn.com', 'foxnews.com', 'cbssports.com', 'cbsnews.com', 'bleacherreport.com']
+domains = ['espn.com', 'foxsports.com', 'cbssports.com', 'sportingnews.com', 'bleacherreport.com']
 
-# Months to search through. February + March 2019
-index_list = ["2019-09","2019-13"]
+# Months to search through. March 2019
+index_list = ["2019-13"]
 
 fs = s3fs.S3FileSystem(anon=True)
 
 def get_text_from_html(html):
     soup = BeautifulSoup(html, 'html.parser')
+    # print(soup.prettify())
+    print(soup.text)
     return soup.get_text()
 
 def search_domain(domain):
@@ -43,7 +45,6 @@ def search_domain(domain):
     
     print("[*] Found a total of %d hits." % len(record_list))
     
-    print(record_list[1])
     return record_list  
 
 def download_page(record):
@@ -54,33 +55,49 @@ def download_page(record):
     prefix = 'https://commoncrawl.s3.amazonaws.com/'
     
     resp = requests.get(prefix + record['filename'], headers={'Range': 'bytes={}-{}'.format(offset, offset_end)})
-    # print(prefix + record['filename'])
-    # resp = requests.get(prefix + record['filename'])
     
-    # The page is stored compressed (gzip) to save space
-    # We can extract it using the GZIP library
-    raw_data = StringIO(resp.content)
+    raw_data = io.BytesIO(resp.content)
     f = gzip.GzipFile(fileobj=raw_data)
     
-    # What we have now is just the WARC response, formatted:
     data = f.read()
     
-    response = ""
-    
-    if len(data):
-        try:
-            warc, header, response = data.strip().split('\r\n\r\n', 2)
-        except:
-            pass
-            
-    return response
+    return data
 
 def main():
+
+    ### PROCESSING STEP 1 - Grab 100 articles from each domain ###
+
     # for domain in domains:
-    #     search_domain(domain)
-    test = search_domain(domains[1])
-    test2 = download_page(test[1])
-    print(test2)
+    #     results = search_domain(domain)
+    #     for i in range(100):
+    #         file = str(domain) + ".txt"
+    #         f = open(file, "a")
+    #         f.write(str(download_page(results[i])))
+    #         f.write("\n\n\n")
+    #         f.close()
+    #         print("Wrote " + domain + " article " + str(i))
+
+    ### PROCESSING STEP 2 - Drop header, turn into valid HTML, and snag text from beautiful soup ###
+    with open('cbssports.com.txt', "r") as f:
+        for line in f:
+            what = line.strip('b\'')
+            huh = what.strip()
+            doc_pos = huh.find('<!DOC')
+            # -1 because of the two newlines in txt file to seperate stuff
+            if doc_pos != -1:
+                html = huh[doc_pos:len(huh)]
+                html = html.replace('\n','')
+                html.strip(" ")
+                html.strip("\t")
+                sort_of_text = get_text_from_html(html)
+                # print(sort_of_text)
+                # f = open('cbssports.txt', "a")
+                # f.write(sort_of_text)
+                # f.close()
+
+
+
+
 
 if __name__== "__main__":
   main()

c1c85a9483c7e54e5e0d318cfe1aefe8fae18b15
manmeet18singh <manmeets@buffalo.edu>
2019-04-03 11:32:51 -0400

    started to adjust forgot to pull so pausing and pulling now

--- src/nyt.py
+++ src/nyt.py
@@ -6,32 +6,41 @@ import time
 api = articleAPI('aruVhl4pt0AUEOnD9LR7V1s4p7wavw9H')
 
 # collect articles with March Madness Keyword
-mmArt = None
-mmUrlCsv = ['url']
+# mmArt = None
+# mmUrlCsv = ['url']
 
-# mmArt = api.search(q = 'March Madness',
-                    # fq = 'The New York Times', page = 70)
+# for pageNum in range(0, 70):
+#     mmArt = api.search(q = 'March Madness',
+#                     fq = 'The New York Times', page = pageNum)
+#     # save mmArticle to csv format
+#     for article in mmArt['response']['docs']:
+#         mmUrlCsv.append(article['web_url'])
+#         time.sleep(6)
 
-# print(mmArt)
+# # save mm data to csv and write to csv in Data/nyt/
+# fileName = os.path.join('../Data/nyt/mmUrl.csv')
+# with open(fileName, 'w') as csvFile:
+#     writer = csv.writer(csvFile)
+#     for url in mmUrlCsv: 
+#         writer.writerow([url])
+# csvFile.close()
 
-# for article in mmArt['response']['docs']:
-    # print(article['web_url'])
+##########################################################################################################################
 
-for pageNum in range(0, 70):
-    mmArt = api.search(q = 'March Madness',
-                    fq = 'The New York Times', page = pageNum)
-    # save mmArticle to csv format
-    for article in mmArt['response']['docs']:
-        mmUrlCsv.append(article['web_url'])
-    time.sleep(6)
+# Collect articles with sport keyword
+sportArt = None
+sportUrlCsv = ['url']
 
 # collect articles with keywords Sport or Sports
-# sportArt = api.search(q = ['Sport', 'Sports'], 
-                        # fq = {'headline' : ['Sport', 'Sports'], 'body':['Sport', 'Sports']}, 
-                        # begin_date = 20190101)
+sportArt = api.search(q = ['Sport', 'Sports'], 
+                        fq = {'headline' : ['Sport', 'Sports'], 'body':['Sport', 'Sports']})
 
-# save data to csv and write to csv in Data/nyt/
-fileName = os.path.join('../Data/nyt/mmUrl.csv')
+
+
+
+
+# save sport article data to csv and write to csv in Data/nyt
+fileName = os.path.join('../Data/nyt/sportUrl.csv')
 with open(fileName, 'w') as csvFile:
     writer = csv.writer(csvFile)
     for url in mmUrlCsv: 

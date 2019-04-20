62123c907b8b6af897c8854a4892cd331a241d2d
TJ <tjchase34@gmail.com>
2019-04-20 12:01:10 -0400

    added coOcc

--- src/co_reducer.py
+++ src/co_reducer.py
@@ -5,13 +5,20 @@ counts = {}
  
 for line in sys.stdin:
     line = line.strip()
-    print(line)
+    # print(line)
     tup, count = line.split(' ', 1)
+    tup1, tup2 = tup.split('|', 1)
+    co = (tup1 + " " + tup2)
+    co2 = (tup2 + " " + tup1)
     
-    if tup in counts:
-        counts[tup] = counts[tup] + count
+    # print(counts)
+    # print(co + " || " + co2)
+    if co in counts:
+        counts[co] = int(counts[co]) + int(count)
+    elif co2 in counts:
+        counts[co2] = int(counts[co2]) + int(count)
     else:
-        counts[tup] = count
+        counts[co] = int(count)
  
 for tup in counts.keys():
-    print(str(tup) + " " + str(counts[tup]))
+    print(tup + " " + str(counts[tup]))
